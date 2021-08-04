2ea59d385fe629be658a6457c275a25f12912a8f
Alexey Frunza <tvoih8r@mail.ru>
2021-08-04 14:49:20 +0300

    production deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :update, :edit, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
   def show
   end
     
@@ -46,5 +49,19 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform this action"
+      redirect_to login_path
+    end
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only delete or edit your own article"
+      redirect_to @article
+    end
   end
 end
